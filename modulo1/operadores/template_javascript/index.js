7365add6a93177977fef7364c853a758efeaed14
jaoska <joao-vts@hotmail.com>
2022-02-15 15:35:40 -0300

    Aula de operadores

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,52 @@
+// Exercício de interpretação 1.
+        
+        /* ("a. ", resultado) false
+           ("b. ", resultado) false
+           ("c. ", resultado) true  
+           ("d. ", typeof resultado) boolean */
+
+// Exercício de interpretação 2.    
+           
+        /* Está faltando converter o prompt com Number e colocar
+           vírgula no final. Será impresso 55 no console. */ 
+
+// Exercício de interpretação 3.
+        /*  Teria que converter com o number antes do prompt e 
+            colocar virgulo no final da mensagem do prompt.*/
+            
+//  Exercícios de escrita de código 1
+
+
+           /* let idade = prompt("Qual a idade do usuário?")
+            let idade2 = prompt("Qual a idade do seu melhor amigo") 
+            console.log(idade>idade2)
+            console.log(idade-idade2) */
+
+
+//   Exercícios de escrita de código 2            
+
+
+            /*let numeroPar = prompt("Insira um número par")
+            console.log (Number(numeroPar%2))*/
+          /*O padrão foi sempre 0 utilizando um número par.
+            A resposta é sempre 1 pois o número 2 é pequeno.*/  
+
+            
+//   Exercícios de escrita de código 3
+          /*let IdadeAnos = prompt("Qual a sua idade?") 
+          console.log(IdadeAnos)
+          let IdadeMeses = IdadeAnos*12
+          console.log(IdadeMeses)
+          let IdadeDias = IdadeAnos*365   
+          console.log(IdadeDias)
+          let IdadeHoras = IdadeAnos*8760
+          console.log(IdadeHoras)*/
+
+
+//   Exercícios de escrita de código 4
+          /*let num1 = prompt("Insira o primeiro número")
+          let num2 = prompt("Insira o segundo número")
+          console.log ("O primeiro numero é maior que o segundo?", num1>num2)
+          console.log ("O primeiro numero é igual ao segundo?", num1===num2)
+          console.log ("O primeiro numero é divisível pelo segundo?", num1%num2)
+          console.log ("O segundo numero é divisível pelo primeiro?", num2%num1)*/
