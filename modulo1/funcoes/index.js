93d67cffa306b6f4a7c0754c9870f5475a45d20e
jaoska <joao-vts@hotmail.com>
2022-02-17 16:57:18 -0300

    Aula de funcao

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,36 @@
+// Exercícios de interpretação de código 1.
+// a) 10 e 50
+// b) daria erro no código
+
+// Exercícios de interpretação de código 2.
+//a) Imprime ao usuário true ou false de acordo com oque ele digitar. No caso "cenoura" seria true.
+//b) Todas as respostas são true pois contém a palavra cenoura e ele sempre vai retornar "cenoura".
+
+
+//Exercícios de escrita de código 1.
+//a)
+
+//console.log("Eu sou João, tenho 27 anos, moro em Bauru e sou estudante.")
+//b)
+
+let nome = 'Joao'
+let idade = 27
+let cidade = 'Bauru'
+let profi = 'Dev'
+
+function Escrever(nome, idade, cidade, profi){
+
+
+const Mensagem = `Eu sou ${nome}, tenho ${idade} anos moro em ${cidade} e sou ${profi}.`
+return Mensagem
+
+}
+const texto = Escrever (nome, idade, cidade, 'Dev')
+
+console.log(texto);
+
+//Exercícios de escrita de código 2.
+
+ 
+
+
