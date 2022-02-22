83e3e0f61526afb388abe4a5745ac80824dc2b44
jaoska <joao-vts@hotmail.com>
2022-02-22 15:05:23 -0300

    exercícios de condicionais

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,68 @@
+//Exercícios de escrita de código 1
+
+// const suaIdade = Number(prompt('Qual a sua idade?'))
+
+
+//     if(suaIdade >= 18){
+//        console.log('Pode dirigir')
+//     }else{
+//         console.log('Não pode dirigir')
+//     }
+
+//Exercícios de escrita de código 2
+
+// const turno = prompt('Que turno do dia você estuda?')
+//  if(turno === 'M'){
+//      console.log('Bom Dia!')
+//  }else if (turno === 'V'){
+//      console.log('Boa Tarde!')
+//  }else if (turno === 'N'){
+//      console.log('Boa Noite!')
+//  }
+ //Exercícios de escrita de código 3     
+
+//  const turno = prompt('Que turno do dia você estuda?') 
+//  switch (turno){
+//      case 'M':
+//          console.log('Bom Dia!')
+//          break
+//      case 'V':
+//          console.log('Boa Tarde!')
+//          break
+//      case 'N':
+//          console.log('Boa Noite!')
+//          break
+//        }  
+  //Exercícios de escrita de código 4   
+  
+//   const genero = prompt('Qual gênero do filme?')
+//   const preço = prompt('Qual o preço do ingresso?')
+//   if(genero === 'fantasia'){
+//      console.log('Bom Filme!')
+//   }else if(preço === '15'){
+//       console.log('Bom Filme!')
+    
+//   }else{
+//       console.log('Escolha outro filme.')
+//   }
+   
+
+
+
+/////////////////////////////////////////////////////////////////////////
+
+
+
+/* Exercícios de interpretação de código 1
+a) Realiza um teste de porcentagem para saber se é um numero par ou impar
+b) Se for par passou no teste se for impar não passou
+c) Para npumeros ímpares 
+*/
+/* Exercícios de interpretação de código 2
+a) Para saber o preço das frutas listadas
+b) 2,25
+c) 5 */ 
+/* Exercícios de interpretação de código 3 
+a) A primeira linha está pedindo para o usuário digitar um número.
+b) 10 = Este número passou no teste -10 = Erro no console
+c) O erro ocorre pois a váriavel let mensagem não comporta nada.  */
