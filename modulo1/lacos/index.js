c14df0e54003516aac04259a78f4595caaac8d3f
jaoska <joao-vts@hotmail.com>
2022-02-23 16:55:11 -0300

    exercícios de laços (com dificuldades rsrs)

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,48 @@
+// Exercícios de interpretação de código 1.
+// Está contando de 0 a 5 e somando o valor que é resultado é 10.
+
+// Exercícios de interpretação de código 2.
+// 19, 21, 23, 25, 27, 30
+//b) Sim.
+    // let indice = 0
+    // const lista = [10.11,12,15,18,19,21,23,25,27,38]
+    // for (let numero of lista){
+    //     if (numero > 18) {
+    //         console.log(numero, "indice:", indice)
+
+    //     }
+    //     indice++
+    // }
+
+
+// Exercícios de escrita de código 1
+
+
+// const nomePets = []
+
+// let numBixos = Number(prompt('Quantos bichinhos de estimação você tem?'))
+//     if(numBixos === 0){
+//         console.log("Que pena! Você pode adotar um pet!")
+
+//     }else if(numBixos > 0){
+//         let i = 0
+//         while(i < numBixos ){
+//             nomePets.push(prompt('Digite o nome do seus pets:'))
+//             i++;
+//         }
+//         console.log(nomePets)
+//     }
+
+     
+
+
+
+
+// Exercícios de escrita de código 2 
+// let arrayOriginal = [80, 30, 130, 40, 60]
+
+//  function imprimirFrase(){}
+
+
+ /* Senti dificuldades com os laços, mas vou rever a aula e os exercícos para entender melhor :( 
+     */
